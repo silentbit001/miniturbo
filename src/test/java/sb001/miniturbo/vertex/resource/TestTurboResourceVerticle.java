faab0a83bcf2b07443cb5d0e4fe7befcd7162e0c
sb001 <silentbit001@gmail.com>
2018-10-25 21:40:52 -0300

    fix build and tests

--- src/test/java/sb001/miniturbo/vertex/resource/TestTurboResourceVerticle.java
+++ src/test/java/sb001/miniturbo/vertex/resource/TestTurboResourceVerticle.java
@@ -3,7 +3,7 @@ package sb001.miniturbo.vertex.resource;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,15 +17,16 @@ import io.vertx.junit5.VertxTestContext;
 @ExtendWith(VertxExtension.class)
 public class TestTurboResourceVerticle {
 
-    @BeforeEach
-    void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+    @BeforeAll
+    @DisplayName("Should start a Web Server on port 8081")
+    static void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
         vertx.deployVerticle(new TurboResourceVerticle(), testContext.succeeding(id -> testContext.completeNow()));
     }
 
     @Test
-    @DisplayName("Should start a Web Server on port 8081")
+    @DisplayName("Should return some resources")
     @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
-    public void testGetAllResources(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    void testGetAllResources(Vertx vertx, VertxTestContext testContext) throws Throwable {
         vertx.createHttpClient().getNow(TurboResourceVerticle.SERVER_PORT, "localhost", "/",
                 response -> testContext.verify(() -> {
                     Assertions.assertTrue(response.statusCode() == 200);
